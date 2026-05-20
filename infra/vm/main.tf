f0d61fca9b04543a122c2a3b6e4f7601db62061d
Milan <deepanshu.v7@gmail.com>
2026-05-20 17:10:11 +0530

    first commit

--- infra/vm/main.tf
+++ infra/vm/main.tf
@@ -88,6 +88,31 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+
+    # 🔥 THIS IS THE MAGIC (Docker auto install)
+  custom_data = base64encode(<<EOF
+#!/bin/bash
+apt-get update -y
+apt-get install -y ca-certificates curl gnupg lsb-release
+
+install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+systemctl enable docker
+systemctl start docker
+
+usermod -aG docker ${var.admin_username}
+EOF
+  )
     lifecycle {
     ignore_changes = [
       admin_password
